b363db1ffc3de71bf3504516f468b9042536a941
刘辉20020013 <20020013@suning.com>
2023-03-31 17:14:19 +0800

    feat: /react-native-gesture-handler+2.6.2.patch

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 /**
  * @format
  */
-
 import { AppRegistry, LogBox } from 'react-native'
-import App from './src/index'
 import { name as appName } from './app.json'
+import App from './src/index'
 
 AppRegistry.registerComponent(appName, () => {
   LogBox.ignoreAllLogs()
